d683419979ea67eb8476882fe6970370fc862f37
{XenoBenn} <{benjamin.komar@gmail.com}>
2025-06-26 10:30:38 +0200

    Adding helper functions to vec3 struct.

--- src/vec3.zig
+++ src/vec3.zig
@@ -11,3 +11,35 @@ pub fn init(x: f32, y: f32, z: f32) Vec3 {
 pub fn length(vec: Vec3) f32 {
     return @sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
 }
+
+pub fn add(v: Vec3, w: Vec3) Vec3 {
+    v.x += w.x;
+    v.y += w.y;
+    v.z += w.z;
+    return v;
+}
+
+pub fn subs(v: Vec3, w: Vec3) Vec3 {
+    v.x -= w.x;
+    v.y -= w.y;
+    v.z -= w.z;
+    return v;
+}
+
+pub fn mul(v: Vec3, w: Vec3) Vec3 {
+    v.x *= w.x;
+    v.y *= w.y;
+    v.z *= w.z;
+    return v;
+}
+
+pub fn scale(v: Vec3, c: f32) Vec3 {
+    v.x *= c;
+    v.y *= c;
+    v.z *= c;
+    return v;
+}
+
+pub fn dot(v: Vec3, w: Vec3) f32 {
+    return .{v.x * w.x + v.y * w.y + v.z * w.z};
+}
